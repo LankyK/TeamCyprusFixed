1c96417087daab4ebcd40859a024319fee060ef9
thanksalot <housemk1@ferris.edu>
2017-10-09 11:27:14 -0400

    asdf

---
+++ Ave, Var, SD/Ave, Var, SD/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ave__Var__SD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
