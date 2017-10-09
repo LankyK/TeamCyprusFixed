5f278cc7a375be261635ee3ba9be6953c0968cd9
thanksalot <housemk1@ferris.edu>
2017-10-09 11:54:53 -0400

    Added input for whole positive and negative numbers

--- Ave, Var, SD/Ave, Var, SD/Program.cs
+++ Ave, Var, SD/Ave, Var, SD/Program.cs
@@ -10,6 +10,64 @@ namespace Ave__Var__SD
     {
         static void Main(string[] args)
         {
+            bool running = true;
+
+            while (running) { 
+                Input();
+                running = PromptDone();
+             }            
+        }
+
+        static int[] Input()
+        {
+            Console.WriteLine("Input 5 whole integers separated by spaces");
+
+            string[] input = Console.ReadLine().Split(null);
+
+            int[] output = new int[input.Length];
+
+            bool success = true;
+
+            if (input.Length != 5)
+                success = false;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (success)
+                success = Int32.TryParse(input[i], out output[i]);
+            }
+
+            if (!success)
+                Console.WriteLine("Invalid Input");
+            else
+                foreach (var item in output)
+                {
+                    Console.WriteLine(item);
+                }
+
+            Console.ReadKey();
+            Console.Clear();
+
+            return output;
+        }
+
+        static bool PromptDone()
+        {
+            Console.WriteLine("Continue? (Y to continue, N to quit)");
+
+            string answer = Console.ReadLine();
+
+            Console.Clear();
+
+            if (answer.ToLower().Contains('n'))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
+
